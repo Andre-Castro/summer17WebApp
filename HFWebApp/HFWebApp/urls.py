96b57dfdac61fae6e76f23b2c59dfc26569265bb
Andre-Castro <acast050@ucr.edu>
2017-06-26 13:23:55 -0700

    added includes and templates

--- HFWebApp/HFWebApp/urls.py
+++ HFWebApp/HFWebApp/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-	url(r'^frontPage', include('frontPage.urls')),
+	url(r'^$', include('frontPage.urls')),
 ]
--- HFWebApp/frontPage/views.py
+++ HFWebApp/frontPage/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("<h3> IP Extractor Tool </h3>")
+	return render(request, 'frontPage/home.html')
+	#return HttpResponse("<h3> IP Extractor Tool </h3>")
