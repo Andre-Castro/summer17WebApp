6e20c58ad12821bc7b638f6caf63aec806a193a2
Andre-Castro <acast050@ucr.edu>
2017-06-29 16:33:13 -0700

    minor changes

--- HFWebApp/forumSelect/views.py
+++ HFWebApp/forumSelect/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
 
 def index(request):
-	return render(request, 'forumSelect/home.html')
+	#file = open('')
+
+	#list elements represent: [name, num, num, num, num]
+	dict = {
+		'forums':[
+			["Wilders", "1002"], 
+			["Offcomm", "2002"],
+			["HackThisSite", "3002"],
+		],
+		'name':"test",
+	}
+	return render(request, 'forumSelect/home.html', dict)
