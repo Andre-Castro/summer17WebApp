39399b3f53ac5d1e596d4a5e337dd4882c0092f6
Andre-Castro <acast050@ucr.edu>
2017-06-30 16:13:17 -0700

    Added user request form

--- HFWebApp/forumSelect/views.py
+++ HFWebApp/forumSelect/views.py
@@ -1,15 +1,5 @@
 from django.shortcuts import render
 
 def index(request):
-	#file = open('')
-
-	#list elements represent: [name, num, num, num, num]
-	dict = {
-		'forums':[
-			["Wilders", "1002"], 
-			["Offcomm", "2002"],
-			["HackThisSite", "3002"],
-		],
-		'name':"test",
-	}
+	dict = {}
 	return render(request, 'forumSelect/home.html', dict)
