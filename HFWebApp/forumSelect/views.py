6795beb30409ed5ce914d539e31175c0a3f247d9
Andre-Castro <acast050@ucr.edu>
2017-07-03 16:56:52 -0700

    add support for postgress for graph

--- HFWebApp/forumSelect/views.py
+++ HFWebApp/forumSelect/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
+import psycopg2
 
 def index(request):
-	dict = {}
+	dict = {
+		"name": "NAME"
+	}
+	'''
+	DBname = "MichalisPrj"
+	pword = "1"
+	tableName = "tbl_wilders_post"
+	con = psycopg2.connect(database=DBname, user = "postgres", password=pword) #connect to postgres
+	curs = con.cursor()
+	ExecuteStatement = "SELECT title FROM " + tableName + " WHERE pdate > '2016-06-01'"
+	curs.execute(ExecuteStatement)
+	pdatesList = curs.fetchall()
+	
+	i = 0
+	for pid in pdatesList:
+		pdatesList[i] = str(pdatesList[i])
+		i+= 1
+	
+	dict["pdates"] = pdatesList
+	'''
+	
 	return render(request, 'forumSelect/home.html', dict)
